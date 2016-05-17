2a3044e279a69a64c207de1db0b724347791f316
黄达明 <674707554@qq.com>
2016-05-17 16:08:59 +0800

    1、添加Json解析类

---
+++ DagolfLa/Utils/JsonHttp.h
@@ -0,0 +1,35 @@
+//
+//  JsonHttp.h
+//  TestUUID
+//
+//  Created by 黄安 on 16/5/17.
+//  Copyright © 2016年 com.huangDM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define PORTOCOL_APP_ROOT_URL   "http://obt.wanda.cn/tmsWanda/"
+
+
+
+@interface JsonHttp : NSObject
+
+typedef void (^GBHEStartBlock)(void);
+typedef void (^GBHEFailedBlock)(id errType);
+typedef void (^GBHECompletionBlock)(id data);
+typedef void (^GBHEBytesRecvBlock)(unsigned long long length, unsigned long long total);
+
+
++ (instancetype)jsonHttp;
+- (void) httpRequest:(NSString *)url
+            withData:(NSDictionary *)postData
+       requestMethod:(NSString*)httpMethod
+         failedBlock:(GBHEFailedBlock)failedBlock
+     completionBlock:(GBHECompletionBlock)completionBlock;
+
+
+//取消网络请求
+- (void)cancelRequest;
+
+
+@end
