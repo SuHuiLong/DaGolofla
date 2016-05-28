8d2153a821ce63c6f99cbaad2a1145a3cbbcdb98
lemonSun <271345526@qq.com>
2016-05-28 20:38:07 +0800

    xd

--- DagolfLa/球队新版本/controller/球队详情/JGTeamMemberORManagerViewController.m
+++ DagolfLa/球队新版本/controller/球队详情/JGTeamMemberORManagerViewController.m
@@ -222,8 +222,8 @@ static CGFloat ImageHeight  = 210.0;
 //        fiData = [NSData dataWithContentsOfURL:[Helper imageUrl:self.ymData.pics[0]]];
 //    }
     
-    NSString*  shareUrl = [NSString stringWithFormat:@"http://www.dagolfla.com:8081/dagaoerfu/html5/mood/Topic_details.html?id=%@",[self.detailDic objectForKey:@""]];
-    [UMSocialData defaultData].extConfig.title=[NSString stringWithFormat:@"来自%@的话题",[self.detailDic objectForKey:@""]];
+    NSString*  shareUrl = [NSString stringWithFormat:@"http://192.168.1.104:8888/imgcache.dagolfla.com/share/team/team.html?key=%@",[self.detailDic objectForKey:@"teamKey"]];
+    [UMSocialData defaultData].extConfig.title=[NSString stringWithFormat:@"来自%@的话题",[self.detailDic objectForKey:@"name"]];
     if (index == 0){
         //微信
         [UMSocialWechatHandler setWXAppId:@"wxdcdc4e20544ed728" appSecret:@"fdc75aae5a98f2aa0f62ef8cba2b08e9" url:shareUrl];
@@ -237,7 +237,7 @@ static CGFloat ImageHeight  = 210.0;
         //朋友圈
         [UMSocialWechatHandler setWXAppId:@"wxdcdc4e20544ed728" appSecret:@"fdc75aae5a98f2aa0f62ef8cba2b08e9" url:shareUrl];
         [UMSocialConfig hiddenNotInstallPlatforms:@[UMShareToWechatSession,UMShareToWechatTimeline,UMShareToSina]];
-        [[UMSocialDataService defaultDataService]  postSNSWithTypes:@[UMShareToWechatTimeline] content:[NSString stringWithFormat:@"来自%@的话题:%@ %@",[self.detailDic objectForKey:@""],[self.detailDic objectForKey:@""],shareUrl] image:fiData location:nil urlResource:nil presentedController:self completion:^(UMSocialResponseEntity *response){
+        [[UMSocialDataService defaultDataService]  postSNSWithTypes:@[UMShareToWechatTimeline] content:[NSString stringWithFormat:@"来自%@的话题:%@ %@",[self.detailDic objectForKey:@"name"],[self.detailDic objectForKey:@""],shareUrl] image:fiData location:nil urlResource:nil presentedController:self completion:^(UMSocialResponseEntity *response){
             if (response.responseCode == UMSResponseCodeSuccess) {
 //                [self shareS:indexRow];
             }
