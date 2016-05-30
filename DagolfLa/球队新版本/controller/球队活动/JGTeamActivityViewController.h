3557de7ac21acf1820a887f859400e0f9d3e441f
黄达明 <674707554@qq.com>
2016-05-30 12:34:21 +0800

    添加备注

--- DagolfLa/球队新版本/controller/球队活动/JGTeamActivityViewController.h
+++ DagolfLa/球队新版本/controller/球队活动/JGTeamActivityViewController.h
@@ -10,7 +10,7 @@
 
 @interface JGTeamActivityViewController : ViewController
 
-@property (nonatomic, assign)NSInteger myActivityList;
+@property (nonatomic, assign)NSInteger myActivityList;//我的球队活动传 －－ 1
 
 
 @end
