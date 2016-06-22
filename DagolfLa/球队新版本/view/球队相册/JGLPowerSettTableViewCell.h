47e9571b6f72a07285f8a63866da733b16c27335
madridlee <963182259@qq.com>
2016-06-22 20:24:20 +0800

    lq

---
+++ DagolfLa/球队新版本/view/球队相册/JGLPowerSettTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  JGLPowerSettTableViewCell.h
+//  DagolfLa
+//
+//  Created by 黄达明 on 16/6/21.
+//  Copyright © 2016年 bhxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGLPowerSettTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *labelTitle;
+@property (weak, nonatomic) IBOutlet UILabel *labelName;
+@property (weak, nonatomic) IBOutlet UIButton *btnChange;
+
+@end
