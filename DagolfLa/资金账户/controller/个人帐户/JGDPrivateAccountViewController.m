f0b5aa27705e6b2451b64bdb82e81798e0b9d2b1
lemonSun <271345526@qq.com>
2016-06-28 11:31:30 +0800

    xd

--- DagolfLa/资金账户/controller/个人帐户/JGDPrivateAccountViewController.m
+++ DagolfLa/资金账户/controller/个人帐户/JGDPrivateAccountViewController.m
@@ -20,7 +20,7 @@
     [super viewDidLoad];
     self.title = @"个人帐户";
     [self creatTableV];
-    
+    self.view.backgroundColor = [UIColor colorWithHexString:@"#EEEEEE"];
     // 去黑线
     UINavigationBar *navigationBar = self.navigationController.navigationBar;
     [navigationBar setBackgroundImage:[UIImage imageNamed:@"parentTopBackgroupd"] forBarPosition:UIBarPositionAny barMetrics:UIBarMetricsDefault];
@@ -32,13 +32,14 @@
 
 
 - (void)creatTableV{
-    self.tableView = [[UITableView alloc] initWithFrame:[UIScreen mainScreen].bounds style:(UITableViewStylePlain)];
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, screenWidth, 289 * ScreenWidth / 375) style:(UITableViewStylePlain)];
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
+    self.tableView.scrollEnabled = NO;
     UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, screenWidth, 160 *ScreenWidth / 375)];
     view.backgroundColor = [UIColor colorWithHexString:@"#32B14D"];
     self.tableView.tableHeaderView = view;
-    self.tableView.backgroundColor = [UIColor colorWithHexString:@"#32B14D"];
+    self.tableView.backgroundColor = [UIColor colorWithHexString:@"#EEEEEE"];
 
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20 * ScreenWidth / 375, 20 * ScreenWidth / 375, 100 * ScreenWidth / 375, 30 * ScreenWidth / 375)];
     label.text = @"帐户余额（元）";
@@ -53,11 +54,16 @@
     balanceLB.textColor = [UIColor whiteColor];
     [view addSubview:balanceLB];
     
-    
+    UIButton *button = [UIButton buttonWithType:(UIButtonTypeSystem)];
+    button.frame = CGRectMake(120 * ScreenWidth / 375, 136 * ScreenWidth / 375, 135 * ScreenWidth / 375, 47 * ScreenWidth / 375) ;
+    button.backgroundColor = [UIColor orangeColor];
+    [button addTarget:self action:@selector(takeMoney) forControlEvents:(UIControlEventTouchUpInside)];
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"sysCell"];
     
     
     [self.view addSubview:self.tableView];
+    [self.view insertSubview:button aboveSubview:self.tableView];
+
 }
 
 
@@ -65,7 +71,7 @@
     if (section == 0) {
         return 1;
     }else{
-        return 10;
+        return 1;
     }
 }
 
@@ -79,7 +85,7 @@
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
     if (section == 0) {
-        return 20;
+        return 30;
     }else{
         return 10;
     }
@@ -98,7 +104,12 @@
         cell.textLabel.text = @"交易记录";
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     }else{
-        cell.textLabel.text = @"CELLLLLLLLLLLLLLL";
+        if (indexPath.row == 0) {
+            cell.textLabel.text = @"银行卡";
+            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        }else{
+            cell.textLabel.text = @"中国银行";
+        }
     }
     
     return cell;
@@ -110,6 +121,19 @@
     }else{
         
     }
+}
+
+
+#pragma mark -----提现
+
+- (void)takeMoney{
+    
+}
+
+#pragma mark -----添加银行卡
+
+- (void)addBank{
+    
 }
 
 - (void)didReceiveMemoryWarning {
