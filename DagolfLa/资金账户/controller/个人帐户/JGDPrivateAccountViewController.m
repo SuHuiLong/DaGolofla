adf706158bd77013f3c49add7030de5eccf5e5ea
lemonSun <271345526@qq.com>
2016-06-28 09:45:08 +0800

    xd

--- DagolfLa/资金账户/controller/个人帐户/JGDPrivateAccountViewController.m
+++ DagolfLa/资金账户/controller/个人帐户/JGDPrivateAccountViewController.m
@@ -8,7 +8,9 @@
 
 #import "JGDPrivateAccountViewController.h"
 
-@interface JGDPrivateAccountViewController ()
+@interface JGDPrivateAccountViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
 
 @end
 
@@ -16,7 +18,98 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.title = @"个人帐户";
+    [self creatTableV];
+    
+    // 去黑线
+    UINavigationBar *navigationBar = self.navigationController.navigationBar;
+    [navigationBar setBackgroundImage:[UIImage imageNamed:@"parentTopBackgroupd"] forBarPosition:UIBarPositionAny barMetrics:UIBarMetricsDefault];
+    [navigationBar setShadowImage:[UIImage new]];
+    
+    
     // Do any additional setup after loading the view.
+}
+
+
+- (void)creatTableV{
+    self.tableView = [[UITableView alloc] initWithFrame:[UIScreen mainScreen].bounds style:(UITableViewStylePlain)];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, screenWidth, 160 *ScreenWidth / 375)];
+    view.backgroundColor = [UIColor colorWithHexString:@"#32B14D"];
+    self.tableView.tableHeaderView = view;
+    self.tableView.backgroundColor = [UIColor colorWithHexString:@"#32B14D"];
+
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20 * ScreenWidth / 375, 20 * ScreenWidth / 375, 100 * ScreenWidth / 375, 30 * ScreenWidth / 375)];
+    label.text = @"帐户余额（元）";
+    label.font = [UIFont systemFontOfSize:14 * ScreenWidth / 375];
+    label.textColor = [UIColor whiteColor];
+    [view addSubview:label];
+    
+    UILabel *balanceLB = [[UILabel alloc] initWithFrame:CGRectMake(10 * ScreenWidth / 375, 40 * ScreenWidth / 375, screenWidth - 20 * ScreenWidth / 375, 80 * ScreenWidth / 375)];
+    balanceLB.text = @"$99999.99";
+    balanceLB.textAlignment = NSTextAlignmentCenter;
+    balanceLB.font = [UIFont systemFontOfSize:40 * ScreenWidth / 375];
+    balanceLB.textColor = [UIColor whiteColor];
+    [view addSubview:balanceLB];
+    
+    
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"sysCell"];
+    
+    
+    [self.view addSubview:self.tableView];
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    if (section == 0) {
+        return 1;
+    }else{
+        return 10;
+    }
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 44 * ScreenWidth / 375;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 2;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    if (section == 0) {
+        return 20;
+    }else{
+        return 10;
+    }
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
+    UIView *view = [[UIView alloc] init];
+    view.backgroundColor = [UIColor colorWithHexString:@"#EEEEEE"];
+    return view;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"sysCell"];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    if (indexPath.section == 0) {
+        cell.textLabel.text = @"交易记录";
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }else{
+        cell.textLabel.text = @"CELLLLLLLLLLLLLLL";
+    }
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (indexPath.section == 0) {
+        
+    }else{
+        
+    }
 }
 
 - (void)didReceiveMemoryWarning {
--- DagolfLa/资金账户/view/个人帐户/JGDPrivateAccountTableViewCell.m
+++ DagolfLa/资金账户/view/个人帐户/JGDPrivateAccountTableViewCell.m
@@ -9,7 +9,25 @@
 
 #import "JGDPrivateAccountTableViewCell.h"
 
+@interface JGDPrivateAccountTableViewCell ()
+
+@property (nonatomic, strong) UIImageView *imageV;
+@property (nonatomic, strong) UILabel *topLB;
+@property (nonatomic, strong) UILabel *nexLB;
+@property (nonatomic, strong) UIButton *deleBtn;
+
+@end
+
 @implementation JGDPrivateAccountTableViewCell
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        
+    }
+    return self;
+}
+
 
 - (void)awakeFromNib {
     [super awakeFromNib];
