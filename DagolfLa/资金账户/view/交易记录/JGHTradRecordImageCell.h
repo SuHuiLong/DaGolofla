9ff7e6ebf3731b6dfdebb0aba10fbba05eec80a3
黄达明 <674707554@qq.com>
2016-06-29 15:09:27 +0800

    1\提现

--- DagolfLa/资金账户/view/交易记录/JGHTradRecordImageCell.h
+++ DagolfLa/资金账户/view/交易记录/JGHTradRecordImageCell.h
@@ -7,7 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+@class JGLBankModel;
 
 @interface JGHTradRecordImageCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *titles;
+
+@property (weak, nonatomic) IBOutlet UILabel *values;
+
+@property (weak, nonatomic) IBOutlet UIImageView *blankImageView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imageLeft;
+
+- (void)configJGLBankModel:(JGLBankModel *)model;
+
 
 @end
