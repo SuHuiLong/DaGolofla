674257b3c2f5585befb1611c43af6463b4970025
黄达明 <674707554@qq.com>
2016-06-29 14:50:22 +0800

    1、交易记录

---
+++ DagolfLa/资金账户/view/交易记录/JGHTradRecordImageCell.h
@@ -0,0 +1,13 @@
+//
+//  JGHTradRecordImageCell.h
+//  DagolfLa
+//
+//  Created by 黄安 on 16/6/29.
+//  Copyright © 2016年 bhxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGHTradRecordImageCell : UITableViewCell
+
+@end
