c93c3f4ea2806979775ac40d2ee839a52f1bb338
lemonSun <271345526@qq.com>
2016-07-04 14:20:15 +0800

    prize

---
+++ DagolfLa/奖项/controller/JGDPrizeViewController.h
@@ -0,0 +1,13 @@
+//
+//  JGDPrizeViewController.h
+//  DagolfLa
+//
+//  Created by 東 on 16/7/4.
+//  Copyright © 2016年 bhxx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface JGDPrizeViewController : ViewController
+
+@end
---
+++ DagolfLa/奖项/view/JGDprizeTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  JGDprizeTableViewCell.h
+//  DagolfLa
+//
+//  Created by 東 on 16/7/4.
+//  Copyright © 2016年 bhxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGDprizeTableViewCell : UITableViewCell
+
+@end
