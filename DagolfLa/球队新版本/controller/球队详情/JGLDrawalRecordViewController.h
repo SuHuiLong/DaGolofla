e16e331a1deef67246986df6036a7a10b04fd476
madridlee <963182259@qq.com>
2016-07-08 10:21:59 +0800

    lq

---
+++ DagolfLa/球队新版本/controller/球队详情/JGLDrawalRecordViewController.h
@@ -0,0 +1,13 @@
+//
+//  JGLDrawalRecordViewController.h
+//  DagolfLa
+//
+//  Created by 黄达明 on 16/7/7.
+//  Copyright © 2016年 bhxx. All rights reserved.
+//
+
+#import "ViewController.h"
+//提现记录
+@interface JGLDrawalRecordViewController : ViewController
+
+@end
