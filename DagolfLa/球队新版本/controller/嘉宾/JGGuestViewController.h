88ec315b4d2d6ff60c75081e1a23733ac50806ba
lemonSun <271345526@qq.com>
2016-08-04 10:36:57 +0800

    guest DDDDDD

---
+++ DagolfLa/球队新版本/controller/嘉宾/JGGuestViewController.h
@@ -0,0 +1,13 @@
+//
+//  JGGuestViewController.h
+//  DagolfLa
+//
+//  Created by 東 on 16/8/4.
+//  Copyright © 2016年 bhxx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface JGGuestViewController : ViewController
+
+@end
