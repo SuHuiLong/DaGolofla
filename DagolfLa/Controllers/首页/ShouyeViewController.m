8ed4240a94d7f49a5a23dc310be8d8e92365dff9
lemonSun <271345526@qq.com>
2016-08-08 17:59:43 +0800

    12345678mall

--- DagolfLa/Controllers/首页/ShouyeViewController.m
+++ DagolfLa/Controllers/首页/ShouyeViewController.m
@@ -372,7 +372,7 @@
                 NSLog(@"shangchen");
 //                UseMallViewController
                 [[NSNotificationCenter defaultCenter] postNotificationName:@"hide" object:self];
-                JGLActivityMemberSetViewController* userVc = [[JGLActivityMemberSetViewController alloc]init];
+                UseMallViewController* userVc = [[UseMallViewController alloc]init];
                 [self.navigationController pushViewController:userVc animated:YES];
             }
                 break;
