16002102d3817bbfc2f7c9a7194b479e4a30b8bb
lemonSun <271345526@qq.com>
2016-09-13 20:16:21 +0800

    DDD

--- DagolfLa/球童/controller/打球人/差杆模式/JGDPlayerScoreCardDetailViewController.m
+++ DagolfLa/球童/controller/打球人/差杆模式/JGDPlayerScoreCardDetailViewController.m
@@ -129,6 +129,11 @@
     allLB.font = [UIFont systemFontOfSize:15 * ProportionAdapter];
     [lightV addSubview:allLB];
     
+    UILabel *tipLB = [[UILabel alloc] initWithFrame:CGRectMake(10 * ProportionAdapter, 5 * ProportionAdapter, 300 * ProportionAdapter, 20 * ProportionAdapter)];
+    tipLB.text = @"注：长按杆数区，进入编辑页，可修改成绩！";
+    tipLB.font = [UIFont systemFontOfSize:12 * ProportionAdapter];
+    [view addSubview:tipLB];
+    
     self.keyLB = [[UILabel alloc] initWithFrame:CGRectMake(170 * ProportionAdapter, 0 * ProportionAdapter, 100 * ProportionAdapter, 45 * ProportionAdapter)];
     self.keyLB.text = [NSString stringWithFormat:@"%@", self.model.invitationCode];
     self.keyLB.textAlignment = NSTextAlignmentLeft;
@@ -227,8 +232,8 @@
         [viewTitle addSubview:self.nameLB];
         
         
-        UILabel *allLB = [[UILabel alloc] initWithFrame:CGRectMake(280 * ProportionAdapter, 108 * ProportionAdapter, 90 * ProportionAdapter, 30)];
-        allLB.text = @"总杆：          杆";
+        UILabel *allLB = [[UILabel alloc] initWithFrame:CGRectMake(270 * ProportionAdapter, 108 * ProportionAdapter, 110 * ProportionAdapter, 30)];
+        allLB.text = @"总差杆：          杆";
         allLB.font = [UIFont systemFontOfSize:12 * ProportionAdapter];
         [viewTitle addSubview:allLB];
         
