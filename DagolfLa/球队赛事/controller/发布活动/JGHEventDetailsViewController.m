22ae8b62fc54824db90f44a77f6cc14647fcbfd9
黄达明 <674707554@qq.com>
2016-10-21 10:19:39 +0800

    34567

--- DagolfLa/球队赛事/controller/发布活动/JGHEventDetailsViewController.m
+++ DagolfLa/球队赛事/controller/发布活动/JGHEventDetailsViewController.m
@@ -18,6 +18,8 @@
 #import "JGLTeamSignUpSuccViewController.h"
 #import "JGLSignSuccFinishViewController.h"
 #import "JGHGameRoundsRulesViewController.h"
+#import "JGDCheckScoreViewController.h"
+#import "JGHGameRoundsRulesViewController.h"
 
 static CGFloat ImageHeight  = 210.0;
 static NSString *const JGHHeaderLabelCellIdentifier = @"JGHHeaderLabelCell";
@@ -192,8 +194,6 @@ static NSString *const JGSignUoPromptCellIdentifier = @"JGSignUoPromptCell";
             }
             
             [self setData];//设置名称 及 图片
-            
-//            [self createApplyBtn:0];
             
             if ([data objectForKey:@"hasSignUp"]) {
                 _isApply = [[data objectForKey:@"hasSignUp"] integerValue];
@@ -421,6 +421,7 @@ static NSString *const JGSignUoPromptCellIdentifier = @"JGSignUoPromptCell";
         JGHHeaderLabelCell *headerCell = [tableView dequeueReusableCellWithIdentifier:JGHHeaderLabelCellIdentifier];
         UIButton *detailsBtn = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, screenWidth, headerCell.frame.size.height)];
         [detailsBtn addTarget:self action:@selector(pushSectionDetailSCtrl:) forControlEvents:UIControlEventTouchUpInside];
+        detailsBtn.tag = 1000 +section;
         [headerCell addSubview:detailsBtn];
         
         if (section == 6) {
@@ -433,10 +434,15 @@ static NSString *const JGSignUoPromptCellIdentifier = @"JGSignUoPromptCell";
                 [headerCell congiftitles:_titleArray[section]];
             }
         }else{
-            headerCell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
             [headerCell congiftitles:_titleArray[section]];
+            if (section == 7) {
+                [headerCell configInvoiceIfo:_model.userName];
+            }else if (section == 8){
+                [headerCell configInvoiceIfo:_model.userMobile];
+            }else{
+                headerCell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            }
         }
-        
         
         return (UIView *)headerCell;
     }
@@ -448,7 +454,23 @@ static NSString *const JGSignUoPromptCellIdentifier = @"JGSignUoPromptCell";
 }
 #pragma mark -- 点击事件
 - (void)pushSectionDetailSCtrl:(UIButton *)btn{
+    NSLog(@"btn.tag == %td", btn.tag -1000);
+    //查看规则
+    if (btn.tag -1000 == 3) {
+        JGHGameRoundsRulesViewController *roundCtrl = [[JGHGameRoundsRulesViewController alloc]init];
+        [self.navigationController pushViewController:roundCtrl animated:YES];
+    }
     
+    if (btn.tag -1000 == 4) {
+        
+    }
+    
+    //查看成绩
+    if (btn.tag -1000 == 5) {
+        JGDCheckScoreViewController *checkCtrl = [[JGDCheckScoreViewController alloc]init];
+        checkCtrl.matchKey = [NSNumber numberWithInteger:_model.timeKey];
+        [self.navigationController pushViewController:checkCtrl animated:YES];
+    }
 }
 #pragma mark - Table View Delegate
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
