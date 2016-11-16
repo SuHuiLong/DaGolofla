def802c9109a8601a1e7fbebcd9193c57436d288
黄达明 <674707554@qq.com>
2016-11-16 10:10:11 +0800

    4354

--- DagolfLa/新首页/view/导航列表/JGHNavListView.m
+++ DagolfLa/新首页/view/导航列表/JGHNavListView.m
@@ -14,34 +14,34 @@
 - (instancetype)initWithFrame:(CGRect)frame{
     if (self == [super initWithFrame:frame]) {
         self.backgroundColor = [UIColor whiteColor];
-        _teamBtn = [[UIButton alloc]initWithFrame:CGRectMake(40 *ProportionAdapter, 6 *ProportionAdapter, 48 *ProportionAdapter, 48 *ProportionAdapter)];
+        _teamBtn = [[UIButton alloc]initWithFrame:CGRectMake(44 *ProportionAdapter, 15 *ProportionAdapter, 49 *ProportionAdapter, 32 *ProportionAdapter)];
         [_teamBtn setImage:[UIImage imageNamed:@"team"] forState:UIControlStateNormal];
         [_teamBtn addTarget:self action:@selector(teamBtnClick:) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:_teamBtn];
         
-        _teamLable = [[UILabel alloc]initWithFrame:CGRectMake(0, 53 *ProportionAdapter, self.frame.size.width/3, 20 *ProportionAdapter)];
+        _teamLable = [[UILabel alloc]initWithFrame:CGRectMake(0, 55 *ProportionAdapter, self.frame.size.width/3, 20 *ProportionAdapter)];
         _teamLable.text = @"球队部落";
         _teamLable.font = [UIFont systemFontOfSize:16 *ProportionAdapter];
         _teamLable.textAlignment = NSTextAlignmentCenter;
         [self addSubview:_teamLable];
         
-        _scoreBtn = [[UIButton alloc]initWithFrame:CGRectMake(162 *ProportionAdapter, 6 *ProportionAdapter, 48 *ProportionAdapter, 48*ProportionAdapter)];
+        _scoreBtn = [[UIButton alloc]initWithFrame:CGRectMake(163 *ProportionAdapter, 15 *ProportionAdapter, 49 *ProportionAdapter, 32*ProportionAdapter)];
         [_scoreBtn setImage:[UIImage imageNamed:@"kaiju"] forState:UIControlStateNormal];
         [_scoreBtn addTarget:self action:@selector(scoreBtnClick:) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:_scoreBtn];
         
-        _scoreLable = [[UILabel alloc]initWithFrame:CGRectMake(self.frame.size.width/3, 53 *ProportionAdapter, self.frame.size.width/3, 20*ProportionAdapter)];
+        _scoreLable = [[UILabel alloc]initWithFrame:CGRectMake(self.frame.size.width/3, 55 *ProportionAdapter, self.frame.size.width/3, 20*ProportionAdapter)];
         _scoreLable.text = @"开局记分";
         _scoreLable.font = [UIFont systemFontOfSize:16 *ProportionAdapter];
         _scoreLable.textAlignment = NSTextAlignmentCenter;
         [self addSubview:_scoreLable];
         
-        _resultsBtn = [[UIButton alloc]initWithFrame:CGRectMake(284 *ProportionAdapter, 6 *ProportionAdapter, 48 *ProportionAdapter, 48 *ProportionAdapter)];
+        _resultsBtn = [[UIButton alloc]initWithFrame:CGRectMake(282 *ProportionAdapter, 15 *ProportionAdapter, 48 *ProportionAdapter, 32 *ProportionAdapter)];
         [_resultsBtn setImage:[UIImage imageNamed:@"chengji-"] forState:UIControlStateNormal];
         [_resultsBtn addTarget:self action:@selector(resultsBtnClick:) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:_resultsBtn];
         
-        _resultsLable = [[UILabel alloc]initWithFrame:CGRectMake(self.frame.size.width/3*2, 53 *ProportionAdapter, self.frame.size.width/3, 20 *ProportionAdapter)];
+        _resultsLable = [[UILabel alloc]initWithFrame:CGRectMake(self.frame.size.width/3*2, 55 *ProportionAdapter, self.frame.size.width/3, 20 *ProportionAdapter)];
         _resultsLable.text = @"历史成绩";
         _resultsLable.font = [UIFont systemFontOfSize:16 *ProportionAdapter];
         _resultsLable.textAlignment = NSTextAlignmentCenter;
