94cb356e0498891daa216c6d9957cd14f0b3ad78
lemonSun <271345526@qq.com>
2017-02-28 14:33:24 +0800

    share

--- DagolfLa/新首页/controller/球场预定/JGDCourtDetailViewController.m
+++ DagolfLa/新首页/controller/球场预定/JGDCourtDetailViewController.m
@@ -137,7 +137,7 @@ static CGFloat ImageHeight  = 210.0;
     
     //咨询
     UIButton *consultBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    consultBtn.frame = CGRectMake(screenWidth - 70 * screenWidth / 320, 17 * ProportionAdapter, 22 * screenWidth / 320, 22 * screenWidth / 320);
+    consultBtn.frame = CGRectMake(screenWidth - 70 * screenWidth / 320, 22 * ProportionAdapter, 22 * screenWidth / 320, 22 * screenWidth / 320);
     consultBtn.titleLabel.font = [UIFont systemFontOfSize:FontSize_Normal];
     [consultBtn setTintColor:[UIColor whiteColor]];
     [consultBtn setImage:[UIImage imageNamed:@"consult"] forState:(UIControlStateNormal)];
@@ -148,7 +148,7 @@ static CGFloat ImageHeight  = 210.0;
     
     // share
     UIButton *shareBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    shareBtn.frame = CGRectMake(screenWidth - 40 * screenWidth / 320, 15 * ProportionAdapter, 23 * screenWidth / 320, 23 * screenWidth / 320);
+    shareBtn.frame = CGRectMake(screenWidth - 35 * screenWidth / 320, 20 * ProportionAdapter, 23 * screenWidth / 320, 23 * screenWidth / 320);
     shareBtn.titleLabel.font = [UIFont systemFontOfSize:FontSize_Normal];
     [shareBtn setTintColor:[UIColor whiteColor]];
     [shareBtn setImage:[UIImage imageNamed:@"ic_portshare"] forState:(UIControlStateNormal)];
@@ -632,13 +632,15 @@ static CGFloat ImageHeight  = 210.0;
     NSString *md5Str = [Helper md5HexDigest:[NSString stringWithFormat:@"ballKey=%@dagolfla.com", self.timeKey]];
     NSString*  shareUrl = [NSString stringWithFormat:@"http://imgcache.dagolfla.com/share/team/bookBallPark.html?ballKey=%@&md5=%@", self.timeKey, md5Str];
     
-    [UMSocialData defaultData].extConfig.title=[NSString stringWithFormat:@"%@",[self.detailDic objectForKey:@"title"]];
+    [UMSocialData defaultData].extConfig.title=[NSString stringWithFormat:@"%@",[self.detailDic objectForKey:@"ballName"]];
+    NSString *timeString =[[NSString stringWithFormat:@"%@", [self.detailDic objectForKey:@"unitPriceDate"]] substringWithRange:NSMakeRange(0, 10)];
+    NSString *detailString = [NSString stringWithFormat:@"%@ | %@洞\n%@  ¥%@" ,[self.detailDic objectForKey:@"type"], [self.detailDic objectForKey:@"holesSum"],timeString, self.unitPrice];
+    ;
     if (index == 0){
-        
         //微信
         [UMSocialWechatHandler setWXAppId:@"wxdcdc4e20544ed728" appSecret:@"fdc75aae5a98f2aa0f62ef8cba2b08e9" url:shareUrl];
         [UMSocialConfig hiddenNotInstallPlatforms:@[UMShareToWechatSession,UMShareToWechatTimeline,UMShareToSina]];
-        [[UMSocialDataService defaultDataService]  postSNSWithTypes:@[UMShareToWechatSession] content:[NSString stringWithFormat:@"%@" ,[self.detailDic objectForKey:@"summary"]]  image:obj location:nil urlResource:nil presentedController:self completion:^(UMSocialResponseEntity *response){
+        [[UMSocialDataService defaultDataService]  postSNSWithTypes:@[UMShareToWechatSession] content:detailString  image:obj location:nil urlResource:nil presentedController:self completion:^(UMSocialResponseEntity *response){
             if (response.responseCode == UMSResponseCodeSuccess) {
                 //                [self shareS:indexRow];
             }
@@ -648,7 +650,7 @@ static CGFloat ImageHeight  = 210.0;
         //朋友圈
         [UMSocialWechatHandler setWXAppId:@"wxdcdc4e20544ed728" appSecret:@"fdc75aae5a98f2aa0f62ef8cba2b08e9" url:shareUrl];
         [UMSocialConfig hiddenNotInstallPlatforms:@[UMShareToWechatSession,UMShareToWechatTimeline,UMShareToSina]];
-        [[UMSocialDataService defaultDataService]  postSNSWithTypes:@[UMShareToWechatTimeline] content:[NSString stringWithFormat:@"%@" ,[self.detailDic objectForKey:@"summary"]] image:obj location:nil urlResource:nil presentedController:self completion:^(UMSocialResponseEntity *response){
+        [[UMSocialDataService defaultDataService]  postSNSWithTypes:@[UMShareToWechatTimeline] content:detailString image:obj location:nil urlResource:nil presentedController:self completion:^(UMSocialResponseEntity *response){
             if (response.responseCode == UMSResponseCodeSuccess) {
                 //                [self shareS:indexRow];
             }
