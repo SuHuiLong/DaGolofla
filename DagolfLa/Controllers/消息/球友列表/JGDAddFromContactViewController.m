30616c4aae80b6b4cfc9357932b4571c857ef480
lemonSun <271345526@qq.com>
2017-03-28 18:00:37 +0800

    DDPhoto

--- DagolfLa/Controllers/消息/球友列表/JGDAddFromContactViewController.m
+++ DagolfLa/Controllers/消息/球友列表/JGDAddFromContactViewController.m
@@ -53,6 +53,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
     
     [self createSearchTable];
     
@@ -371,7 +372,7 @@
     if ([self.listArray[indexPath.section][indexPath.row] isAppUser] == 0) {
         // 邀请
 
-            [self showMessageView:@[[self.listArray[indexPath.section][indexPath.row] mobile]] title:@"" body:@"嗨！我正在使用君高高尔夫APP，感觉很不错，推荐你下载使用。下载链接：https://itunes.apple.com/cn/app/君高高尔夫-打造专业的高尔夫社群平台/id1056048082?mt=8"];
+            [self showMessageView:@[[self.listArray[indexPath.section][indexPath.row] mobile]] title:@"" body:@"嗨！我正在使用君高高尔夫APP，感觉很不错，推荐你下载使用。下载链接：https://itunes.apple.com/cn/app/id1056048082?mt=8"];
         
     }else{
         if ([self.listArray[indexPath.section][indexPath.row] isFriend] == 0) {
@@ -425,7 +426,7 @@
     }
     else if(result==MessageComposeResultCancelled)
     {
-        [LQProgressHud showMessage:@"发短信取消"];
+//        [LQProgressHud showMessage:@"发短信取消"];
     }
     else if(result==MessageComposeResultFailed)
     {
